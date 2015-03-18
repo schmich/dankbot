3a1f434b6a46ba0b0e3c74249f9ff076cace061f
Chris Schmich <schmch@gmail.com>
2015-03-18 06:58:21 -0500

    Tweak respects behavior.

--- plugins/respects.js
+++ plugins/respects.js
@@ -5,18 +5,26 @@ var common = require('../common'),
 
 module.exports = function() {
   var lastF = {};
+  var countF = {};
 
-  return new Command(/^\s*F\s*$/, function() {
+  return new Command(/^\s*F\s*$/i, function() {
     var self = this;
-    var last = lastF[this.channel];
-    var now = +Date.now();
 
-    if (!last || (now - last > (30 * 1000))) {
-      setTimeout(function() {
-        self.say('F');
-      }, 5000);
+    var channel = this.channel;
+    var last = lastF[channel] || 0;
+    var now = Date.now();
 
-      lastF[this.channel] = now;
+    if (now - last > (30 * 1000)) {
+      countF[channel] = countF[channel] || 0;
+      countF[channel]++;
+
+      if (countF[channel] == 2) {
+        setTimeout(function() {
+          self.say('F');
+          lastF[channel] = Date.now();
+          countF[channel] = 0;
+        }, 1000);
+      }
     }
   });
 };
