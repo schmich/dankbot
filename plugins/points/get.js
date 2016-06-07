0e253f877e0969ab1107f464b1086027a93058e3
Chris Schmich <schmch@gmail.com>
2016-06-07 01:22:38 -0500

    Add point querying by rank.

--- plugins/points/get.js
+++ plugins/points/get.js
@@ -13,7 +13,24 @@ module.exports = function(points) {
       return;
     }
 
-    var stats = await(points.query(user));
-    this.say('%s has %s (rank %d)', user, dkp(stats.points), stats.rank);
+    var stats = null;
+    if (user.match(/^\d+$/)) {
+      var position = +user;
+      if (position <= 0) {
+        this.say('%s: you must specify a rank of 1 or higher.', this.user);
+        return;
+      } else {
+        stats = await(points.queryPosition(position));
+      }
+    } else {
+      stats = await(points.query(user));
+    }
+
+    if (!stats) {
+      this.say('%s: unknown user or rank %s.', this.user, user);
+      return;
+    }
+
+    this.say('%s has %s (rank %d)', stats.user, dkp(stats.points), stats.rank);
   });
 };
